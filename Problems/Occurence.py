fb79c3d2fc820a2c8c4e7801c0e6069e8fa0e5ed
CyberMage7 <technician9012@gmail.com>
2024-07-14 23:20:10 +0530

    Added strings

---
+++ Problems/Occurence.py
@@ -0,0 +1,4 @@
+# Occurence of $ in a string
+
+str = "$I am learning $Python and will macha tabhai"
+print("The count of $ in", str, "is", str.count("$"))
---
+++ Strings.py
@@ -0,0 +1,15 @@
+a = "Vishwas"
+b = "Mishra"
+
+c = a + " " + b
+print("The length of", a, "is", len(a), "and", b, "is", len(b))
+print("The length of", c, "is", len(c))
+
+# Slicing :
+# str[startingIndex:endingIndex] (endingIndex not included)
+
+print(a[0 : 4])
+
+# print(a[ : 4]) a[0 : 4]
+# print(a[0 : ]) a[0 : len(a)]
+
