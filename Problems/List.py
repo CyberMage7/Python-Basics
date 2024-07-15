1fd950ac054a3c83c1f0b06936572bdf3609a72a
CyberMage7 <technician9012@gmail.com>
2024-07-15 11:56:02 +0530

    Added List and Tuples

---
+++ Problems/List.py
@@ -0,0 +1,7 @@
+text1 =["@", "123", "456", "123", "@", "7"]
+text2 = text1.copy()
+text1.reverse()
+if(text1 == text2):
+    print("A Palindrome string")
+else:
+    print("Not Palindrome")
---
+++ Problems/Tuples.py
@@ -0,0 +1,6 @@
+tup = ("C","D","A","A","B","B","A")
+print("The count of A in tuple", tup, "is", tup.count("A"))
+
+li = ["C","D","A","A","B","B","A"]
+li.sort()
+print(li)
