303c7b2fe21c1d7cae98c5a9df46c1c9ec083ad7
CyberMage7 <technician9012@gmail.com>
2024-07-16 11:58:28 +0530

    Added practice questions of set and dictionary

---
+++ Problems/Dictionary.py
@@ -0,0 +1,16 @@
+p1 = {
+    "table":("a piece of furniture", "list of facts & figures"), 
+    "cat":"a small animal",
+}
+
+# print(p1)
+
+p2 = {}
+marks1 = float(input("Enter subject of first marks:"))
+marks2 = float(input("Enter subject of second marks:"))
+marks3 = float(input("Enter subject of third marks:"))
+p2["Physics"] = marks1
+p2["Chemkstry"] = marks2
+p2["Maths"] = marks3
+
+print(p2)
---
+++ Problems/Set.py
@@ -0,0 +1,3 @@
+p1 = {"python", "java", "C++", "javascript", "java", "python", "java", "C++", "C"}
+
+print("The Number of classrooms required are:", len(p1))
