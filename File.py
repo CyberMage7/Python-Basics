8a21120754c099237d8293185b739df140b93616
CyberMage7 <technician9012@gmail.com>
2024-07-17 13:34:26 +0530

    Added file code

---
+++ File.py
@@ -0,0 +1,16 @@
+# r-> read the data
+# w->overwrites the entire file
+# a->adds to the file without harming the previous data 
+
+f = open("demo.txt", 'r')
+# data = f.read()
+# print(data)
+
+line1 = f.readline()
+print(line1)
+
+line2 = f.readline()
+print(line2)
+
+line3 = f.readline()
+print(line3)
